d4f3a23b3f8d9d79236110c6d88f487e01783d7c
Antonio Belan <abelan@okta-solutions.com>
2026-03-23 13:30:59 +0500

    KS1-2: решил задачу с количеством работников

---
+++ src/main/kotlin/ks1-1.kt
@@ -0,0 +1,19 @@
+
+
+fun main(){
+    // 1. Переменные из задачи 1
+    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
+    var countOfOrders: Int = 75
+
+    // 2. Вывод всех объявленных ранее переменных
+    println(thanksForOrder)
+    println(countOfOrders)
+
+    // 3. Решаем задачу с работниками
+    var numberOfWorkingPeople : Int = 2000
+    println(numberOfWorkingPeople)
+    // один человек уволился
+    numberOfWorkingPeople = 1999
+    println(numberOfWorkingPeople)
+
+}
