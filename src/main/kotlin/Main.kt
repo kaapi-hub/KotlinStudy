57e72b6791bce21e6953ac28a7b5bcd6450a5383
Antonio Belan <abelan@okta-solutions.com>
2026-03-23 12:11:58 +0500

    Урок 1. Настроил git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
+//package org.example
+//
+////TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
+//// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
+//fun main() {
+//    val name = "Kotlin"
+//    //TIP Нажмите <shortcut actionId="ShowIntentionActions"/>, когда курсор находится на подсвеченном тексте,
+//    // чтобы увидеть предложения IntelliJ IDEA по исправлению.
+//    println("Hello, " + name + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Нажмите <shortcut actionId="Debug"/> для начала отладки кода. Мы установили одну <icon src="AllIcons.Debugger.Db_set_breakpoint"/> точку останова
+//        // для вас, но вы всегда можете добавить больше, нажав <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//    }
+//}
+
 //fun main(args: Array<String>) {
 //    println("Hello World!")
 //
@@ -7,10 +24,14 @@
 //}
 
 fun main(){
-    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
-    var countOfOrders: Int = 75
-
-    println(thanksForOrder)
-    println(countOfOrders)  // Выведет: 75
-
+//    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
+//    var countOfOrders: Int = 75
+//
+//    println(thanksForOrder)
+//    println(countOfOrders)  // Выведет: 75
+    var numberOfWorkingPeople : Int = 2000
+    println(numberOfWorkingPeople)
+    // один человек уволился
+    numberOfWorkingPeople = numberOfWorkingPeople - 1
+    println(numberOfWorkingPeople)
 }
