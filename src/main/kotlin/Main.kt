b5e9d4c3a317a878c68cbcb353ae45fb1008bc7a
Antonio Belan <abelan@okta-solutions.com>
2026-03-23 12:17:10 +0500

    Урок 1. Настроил git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,4 @@
-//package org.example
-//
-////TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
-//// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
-//fun main() {
-//    val name = "Kotlin"
-//    //TIP Нажмите <shortcut actionId="ShowIntentionActions"/>, когда курсор находится на подсвеченном тексте,
-//    // чтобы увидеть предложения IntelliJ IDEA по исправлению.
-//    println("Hello, " + name + "!")
-//
-//    for (i in 1..5) {
-//        //TIP Нажмите <shortcut actionId="Debug"/> для начала отладки кода. Мы установили одну <icon src="AllIcons.Debugger.Db_set_breakpoint"/> точку останова
-//        // для вас, но вы всегда можете добавить больше, нажав <shortcut actionId="ToggleLineBreakpoint"/>.
-//        println("i = $i")
-//    }
-//}
 
-//fun main(args: Array<String>) {
-//    println("Hello World!")
-//
-//    // Try adding program arguments via Run/Debug configuration.
-//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//    println("Program arguments: ${args.joinToString()}")
-//}
 
 fun main(){
 //    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
