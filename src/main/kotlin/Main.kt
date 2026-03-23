66b2a185ecf7601a4fc6d521cd273f54f67afcdb
Antonio Belan <abelan@okta-solutions.com>
2026-03-23 13:10:55 +0500

    KS1-1: Настроил git, привёл код в соответствие с комментариями после ревью

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,10 @@
 
 
 fun main(){
-//    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
-//    var countOfOrders: Int = 75
-//
-//    println(thanksForOrder)
-//    println(countOfOrders)  // Выведет: 75
-    var numberOfWorkingPeople : Int = 2000
-    println(numberOfWorkingPeople)
-    // один человек уволился
-    numberOfWorkingPeople = numberOfWorkingPeople - 1
-    println(numberOfWorkingPeople)
+    val thanksForOrder: String = "Спасибо! Без вас мы бы разорились"
+    var countOfOrders: Int = 75
+
+    println(thanksForOrder)
+    println(countOfOrders)  // Выведет: 75
+
 }
