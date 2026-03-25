cbe2935beb9a73203726165103278b51beef02d0
Antonio Belan <abelan@okta-solutions.com>
2026-03-25 17:54:46 +0500

    KS-2-1: исправил замечания после ревью

--- src/main/kotlin/KS-2-1.kt
+++ src/main/kotlin/KS-2-1.kt
@@ -1,10 +1,10 @@
-fun main(){
-    val student1 = 3
-    val student2 = 4
-    val student3 = 3
-    val student4 = 5
+fun main() {
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+    val quantityOfStudents = 4
 
-    val score = (student1+student2+student3+student4)/4.0
+    val score = ((mark1 + mark2 + mark3 + mark4) / quantityOfStudents).toDouble()
     println(String.format("%.2f", score))
-
 }
