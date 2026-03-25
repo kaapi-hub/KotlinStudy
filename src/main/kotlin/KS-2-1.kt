c6ce049b2bc135d09486445207c37d1a2399b7dc
Antonio Belan <abelan@okta-solutions.com>
2026-03-25 07:46:19 +0500

    KS-2-1: вывод среднего арифметического с 2 знаками после запятой

---
+++ src/main/kotlin/KS-2-1.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val score = (student1+student2+student3+student4)/4.0
+    println(String.format("%.2f", score))
+
+}
